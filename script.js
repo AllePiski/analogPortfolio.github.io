db5a4a123668333d6c93d4556b27e7c9046c8370
Alessandro Piscitiello <pisci@192.168.1.52>
2023-04-17 12:00:27 +0200

    prova generale

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 "use strict";
 
 var slideIndex = 1;
-showBWDivs(slideIndex);
-showColorDivs(slideIndex);
+showDivs(slideIndex, "BW");
+showDivs(slideIndex, "Color");
 
-function scorriBW(n) {
-  showBWDivs((slideIndex += n));
-}
-function scorriColor(n) {
-  showColorDivs((slideIndex += n));
+function scorri(n, strControl) {
+  showDivs((slideIndex += n), strControl);
 }
 
+function showDivs(n, strControl) {
+  var i;
+  if (strControl === "Color") {
+    var x = document.getElementsByClassName("slideImgColor");
+    if (n > x.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = x.length;
+    }
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+  } else {
+    var x = document.getElementsByClassName("slideImgBW");
+    if (n > x.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = x.length;
+    }
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+  }
+}
+
+/*
 function showBWDivs(n) {
   var i;
   var x = document.getElementsByClassName("slideImgBW");
@@ -26,6 +53,7 @@ function showBWDivs(n) {
   x[slideIndex - 1].style.display = "block";
 }
 
+
 function showColorDivs(n) {
   var i;
   var x = document.getElementsByClassName("slideImgColor");
@@ -40,3 +68,4 @@ function showColorDivs(n) {
   }
   x[slideIndex - 1].style.display = "block";
 }
+*/
